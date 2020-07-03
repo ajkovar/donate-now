f4d9dafaf610cffecb7b376da4ab6dd7f988d374
Alex Kovar <ajkovar@gmail.com>
2020-07-02 21:35:11 -0500

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React, { useRef, useEffect, useState } from "react";
-// import "./App.css";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: papayawhip;
+`;
 
 function App() {
-	return <div>Hello world</div>;
+  return <Wrapper>Hello world</Wrapper>;
 }
 
 export default App;
